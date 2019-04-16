87fe342946416746d89985efe06d36e4cf815b70
paramagicdev <konnor5456@gmail.com>
2019-04-16 15:05:44 -0400

    removed sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: %i[new create show]
