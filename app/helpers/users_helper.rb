23e5fe5cdbef0e28f24b2cc306a084341ba585ee
paramagicdev <konnor5456@gmail.com>
2019-04-16 15:46:18 -0400

    implementing login functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def login(user)
+    cookies.signed[:user_id] = user.id
+    cookies.signed[:user_name] = user.name
+  end
+
+  def logout(user)
+
+  end
+
+  def current_user
+    if cookies[:user_id] || cookies[:user_name]
+      User.find_by(id: cookies[:user_id]) || User.find_by(name: cookies[:user_name]
+    end
+  end
 end
