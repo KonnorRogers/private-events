23393a6255d35c385a2a0bf15d0a3eba13b60d21
paramagicdev <konnor5456@gmail.com>
2019-04-19 02:37:07 -0400

    added migration for a creator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# models an event like a birthday party etc
 class Event < ApplicationRecord
+  belongs_to :user, class_name: 'Creator',
+                    foreign_key: 'creator_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false },
                    length: { maximum: 50 }
