5fb6c8bc4dba234aef6e48271a9fbb664c7ff183
paramagicdev <konnor5456@gmail.com>
2019-04-19 03:19:20 -0400

    on creating routes for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,17 @@
 class EventsController < ApplicationController
-  def new
-  end
-
   def create
+    current_user.events.build(events_params)
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def events_params
+    params.require(:event)
   end
 end
